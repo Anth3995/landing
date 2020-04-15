c3db690388d385c46ee4231b158842c7a81af353
Aleksey <morenaleksey@gmail.com>
2020-04-15 22:09:57 +0300

    add responsiv

--- js/timer.js
+++ js/timer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+var countDownDate = new Date("April 17, 2020 0:0:0").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -17,8 +17,8 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("timer").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  document.getElementById("timer").innerHTML = days + "д  : " + hours + "ч : "
+  + minutes + "м : " + seconds + "с";
 
   // If the count down is finished, write some text
   if (distance < 0) {
