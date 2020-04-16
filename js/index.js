eed6a49da867308406de6d5950fb6ff462089a2a
Aleksey <morenaleksey@gmail.com>
2020-04-16 22:59:24 +0300

    rease

--- js/index.js
+++ js/index.js
@@ -1,3 +1,37 @@
 $( document ).ready(function() {
     new WOW().init();
-  });
+ });
+
+$(document).ready(function() {
+
+  	$('#Reviews').slick({
+    	autoplay: true,
+    	mobileFirst: true,
+    	dots: true,
+    	swipeToSlide: true,
+    	arrows: false,
+    	responsive: [
+    		{
+      			breakpoint: 650,
+      			settings: {
+        			arrows: true,
+        			// centerMode: true,
+        			centerPadding: '40px',
+        			slidesToShow: 2,
+        			dots: true
+      			}
+    		},
+    		{
+      			breakpoint: 800,
+      			settings: {
+        			arrows: true,
+        			// centerMode: true,
+        			centerPadding: '40px',
+        			slidesToShow: 3,
+        			dots: true
+      			}
+    		}
+    	]
+
+  	});
+});
